00f3f4b666d0c41d981577fe2c8dc82a460dfe00
parseltongist <>
2021-10-30 20:43:50 +0200

    stage 4/5

--- scraper.py
+++ scraper.py
@@ -1,48 +1,63 @@
+import string
+
 import requests
 from bs4 import BeautifulSoup
 
 
-def takeurl():
-    return input()
+class NatureScraper:
+    def __init__(self):
+        self._url = 'https://www.nature.com/nature/articles?sort=PubDate&year=2020&page=3'
+        self._page_source = None
+        self._articles = []
+        self._saved_articles = []
+
+    def get_page_source(self):
+        r = requests.get(self._url)
+        if r.status_code == requests.codes.ok:
+            self._page_source = BeautifulSoup(r.content, 'html.parser')
+            return self._page_source
+
+    def get_articles(self):
+        self._articles = self._page_source.find_all('article')
+        return self._articles
+
+    def find_news_articles(self):
+        for article in self._articles:
+            article_type_span = article.find('span', attrs={'data-test': 'article.type'})
+            if article_type_span is not None:
+                if article_type_span.contents[1].get_text() == 'News':
+                    article_title = article.find('a').get_text()
+                    for p in string.punctuation:
+                        article_title.maketrans(p, ' ')
+                    article_title = '_'.join(article_title.split(' '))
+                    link_info = {'title': article_title,
+                                 'link': article.find('a')['href']}
+                    self._saved_articles.append(link_info)
+        return self._saved_articles
+
+    def save_news_articles(self):
+        for article in self._saved_articles:
+            filename = article['title'] + '.txt'
+            file = open(filename, 'wb')
+            r = requests.get('https://nature.com' + article['link'])
+            if r.status_code == requests.codes.ok:
+                soup = BeautifulSoup(r.content, 'html.parser')
+                file.write(bytes(soup.find('div', 'c-article-body').get_text().strip(), 'utf-8'))
+            file.close()
 
 
-def obtain_quote():
-    url_to_json = requests.get(user_url, headers={'Accept-Language': 'en-US,en;q=0.5'}).json()
-    try:
-        content = url_to_json['content']
-    except KeyError:
-        print("Invalid quote resource!")
-    else:
-        print(content)
+def main():
+    scraper = NatureScraper()
+    scraper.get_page_source()
+    scraper.get_articles()
+    articles = scraper.find_news_articles()
+    titles = []
+    for a in articles:
+        titles.append(a['title'])
+    scraper.save_news_articles()
+    print('Saved articles:')
+    print(titles)
 
 
-def obtain_desctiption():
-
-    if "www.imdb.com/title/" not in user_url:
-        print("Invalid movie page!")
-    else:
-        r = requests.get(user_url, headers={'Accept-Language': 'en-US,en;q=0.5'})
-        soup = BeautifulSoup(r.content, 'html.parser')
-        # print(soup.prettify())
-        title = soup.find('title').text
-        movie_desctiption = soup.find('meta', {"name": "description"}).get('content')
-        movie_data = {"title": title, "description": movie_desctiption}
-
-        print(movie_data)
-
-
-def save_html():
-    r = requests.get(user_url, headers={'Accept-Language': 'en-US,en;q=0.5'})
-    #print(r.content)
-    with open('source.html', 'wb') as file:
-        file.write(r.content)
-    print("Content saved.")
-
-if __name__ == "__main__":
-    user_url = takeurl()
-    # user_url = "https://www.imdb.com/title/tt0080684/"
-    url_status = requests.get(user_url).status_code
-    if url_status == 200:
-        save_html()
-    else:
-        print(f"The URL returned {url_status}")
+if __name__ == '__main__':
+    main()
