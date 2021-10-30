fd7834da8a6a85be0267ee0a50f0908cc0327179
parseltongist <>
2021-10-30 16:22:20 +0200

    stage 3/5

--- scraper.py
+++ scraper.py
@@ -31,11 +31,18 @@ def obtain_desctiption():
         print(movie_data)
 
 
+def save_html():
+    r = requests.get(user_url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+    #print(r.content)
+    with open('source.html', 'wb') as file:
+        file.write(r.content)
+    print("Content saved.")
+
 if __name__ == "__main__":
     user_url = takeurl()
     # user_url = "https://www.imdb.com/title/tt0080684/"
     url_status = requests.get(user_url).status_code
     if url_status == 200:
-        obtain_desctiption()
+        save_html()
     else:
-        print("no response!")
+        print(f"The URL returned {url_status}")
