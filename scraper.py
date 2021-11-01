522f089c588d0d5a1edb85804620805ea2b1fba8
parseltongist <>
2021-11-01 13:05:10 +0100

    stage 5/5 v1.1.1 final

--- scraper.py
+++ scraper.py
@@ -1,20 +1,21 @@
 import string
 import os
-import os.path
 
 import requests
 from bs4 import BeautifulSoup
 
 
 class NatureScraper:
-    def __init__(self, _last_page, _article_type):
-        self._curr_page_num = 1
-        self._url = f'https://www.nature.com/nature/articles?sort=PubDate&year=2020&page={self._curr_page_num}'
-        self._last_page = _last_page
+    def __init__(self, last_page, _article_type):
+        self.curr_page_num = 1
+        self._url = f'https://www.nature.com/nature/articles?sort=PubDate&year=2020&page={self.curr_page_num}'
+        self.last_page = last_page
         self._article_type = _article_type
         self._page_source = None
         self._articles = []
         self._saved_articles = []
+        self.main_directory = os.getcwd()
+        self.subfolder = None
 
     def get_page_source(self):
         r = requests.get(self._url)
@@ -51,47 +52,34 @@ class NatureScraper:
             file.close()
 
     def page_switch(self):
-        self._curr_page_num += 1
+        self.curr_page_num += 1
 
-
-def folder_maker():
-    def wrapper():
-        ...
-    ...
+    def make_subfolder(self):
+        self.subfolder = "Page_" + str(self.curr_page_num)
+        try:
+            os.mkdir(self.subfolder)
+        except FileExistsError:
+            pass
 
 
 def main():
     try:
         scraper = NatureScraper(int(input("How many pages scrapper should look for?\n")), input("What article type are you looking for?\n").capitalize())
-
     except ValueError:
         print("Only integers accepted as a page numbers")
         return main()
 
-    while scraper._curr_page_num <= scraper._last_page:
-
-        main_directory = os.getcwd()
-        # print("you currently in:", main_directory)
-        # create new folder:
-        subfolder = "Page_" + str(scraper._curr_page_num)
-        try:
-            os.mkdir(subfolder)
-            os.chdir(os.path.join(main_directory, subfolder))
-        except FileExistsError:
-            os.chdir(os.path.join(main_directory, subfolder))
-        # print("you switched to:", os.getcwd())
+    while scraper.curr_page_num <= scraper.last_page:
+        scraper.make_subfolder()
+        os.chdir(scraper.subfolder)
         scraper.get_page_source()
         scraper.get_articles()
         articles = scraper.find_news_articles()
-        titles = []
-        for a in articles:
-            titles.append(a['title'])
+        titles = [article['title'] for article in articles]
         scraper.save_news_articles()
-        print('Saved articles:')
-        print(titles)
-
-        os.chdir(main_directory)
+        print('Saved articles:\n', titles)
         scraper.page_switch()
+        os.chdir(scraper.main_directory)
 
 
 if __name__ == '__main__':
